ac0b628d0ceb465e13a9a1d34eddb746aa9ab1dc
gengjiahao10189 <1902492516@qq.com>
2022-02-22 22:54:47 +0800

    push test1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
         System.out.println("hello hot-fix");
         System.out.println("halo master");
         System.out.println("halo hot-fix");
-        System.out.println("pull test");
+        System.out.println("push test");
     }
 }
