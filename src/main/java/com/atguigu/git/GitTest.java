6d73aa5d0c2cf69f57eb78d1bc4c41865291ba39
gengjiahao10189 <1902492516@qq.com>
2022-02-22 22:09:04 +0800

    hot-fix-commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("halo 2022");
+        System.out.println("hello hot-fix");
     }
 }
